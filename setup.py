993c9d292712e156b06ff0d2fe063a1da62e3cce
eruizgar91 <enrique@keyko.io>
2020-04-28 10:39:24 +0200

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -82,6 +82,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/keyko-io/nevermind-gateway-events',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
